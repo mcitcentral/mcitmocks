200cf02e3ffd6cb02b3d2daf8709399d665e68f6
wilsonplau <hello@wilsonplau.com>
2021-04-20 09:23:29 +0900

    feat: implement header

--- src/main/client/src/components/Header/Header.tsx
+++ src/main/client/src/components/Header/Header.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { User } from "@types";
+import "./Header.scss";
+
+export interface HeaderProps {
+  user: User | null;
+}
 
 const Header: React.FC<{}> = () => {
   return (
     <header className="header">
-      <div className="header__left"> _mcitmocks</div>
+      <div className="header__left">
+        <span>_mcitmocks</span>
+      </div>
     </header>
   );
 };
